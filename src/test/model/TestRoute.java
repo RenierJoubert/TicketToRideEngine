06c9e5fe2443ed63644663d1a420fe988ec7efdc
Renier Joubert <rjoube01@students.cs.ubc.ca>
2026-02-10 13:30:00 -0800

    Added tests for route class

--- src/test/model/TestRoute.java
+++ src/test/model/TestRoute.java
@@ -1,7 +1,6 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +8,16 @@ public class TestRoute {
     
     @BeforeEach
     void runBefore() {
-        
+        Route r1 = new Route("Vancouver", "Seattle", 1);
+        Route r2 = new Route("Los Angeles", "Las Vegas", 2);
     }
 
     @Test
-    void sampleTest() {
-        assertTrue(true);
+    void testRouteConstructor() {
+        assertEquals("Vancouver", r1.getStartCity());
+        assertEquals("Seattle", r1.getEndCity());
+        assertEquals(1, r1.getLength());
     }
 }
+
+
