d2e03bc3ecbf490b2daf528ece0149adeb31c318
Renier Joubert <rjoube01@students.cs.ubc.ca>
2026-02-10 17:05:14 -0800

    Added specification for Hand class

---
+++ src/main/model/Hand.java
@@ -0,0 +1,47 @@
+package model;
+
+// The Hand class represents a list of tickets that the player wishes to complete,
+// the players hand stores a colleciton of tickets and can evaluate the score of the player
+// based on the completion status of each ticket.
+
+import java.util.List;
+
+public class Hand {
+
+    private List<Ticket> tickets;
+
+    // EFFECTS: constructs an empty hand with no tickets
+    public Hand() {
+
+    }
+
+    // REQUIRES: ticket != null
+    // MODIFIES: this
+    // EFFECTS: adds ticket to hand
+    public void addTicket() {
+
+    }
+
+    // REQUIRES: ticket != null
+    // MODIEFIES: this
+    // EFFECTS: removes ticket from hand
+    public void removeTicket() {
+
+    }
+
+    // EFFECTS: returns number of tickets in hand
+    public int size() {
+        return 0;
+    }
+
+    // EFFECTS: returns the total score of the hand
+    public int getScore() {
+        return 0;
+    }
+
+    // EFFECTS: returns true is hand contains no tickets
+    public boolean isEmpty() {
+        return false;
+    }
+
+}
