77dfc5b199e464ee657f0a2492f04bd64cd48969
Renier Joubert <rjoube01@students.cs.ubc.ca>
2026-02-25 22:55:26 -0800

    Added Reader class specifications

---
+++ src/main/persistence/Reader.java
@@ -0,0 +1,45 @@
+package persistence;
+
+import model.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import org.json.*;
+
+// The Reader class reads the player's hand and saved path data from the stored JSON file
+
+public class Reader {
+    private String saveLocation;
+
+    // EFFECTS: constructs a reader to read from the saved file
+    public Reader(String saveLocation) {
+
+    }
+
+    // EFFECTS: reads the saved Hand from file and returns it
+    public Hand read() {
+        return null;
+    }
+
+
+    // EFFECTS: returns the saved file as a string
+    private String fileToString(String saveFile) {
+        return "";
+    }
+
+    // EFFECTS: returns parsed Hand from JSON 
+    private Hand parseHand(JSONObject json) {
+        return null;
+    }
+
+    // MODIFIES: Hand
+    // EFFECTS: Parses tickets from the JSON data and adds them to the user's hand
+    private void getTickets(Hand hand, JSONObject json) {
+
+    }
+
+
+
+}
